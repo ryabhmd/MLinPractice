e99d0214a2caa1367116eca908020a0375b77734
thawab-alkhiami <eng.thkhiami@gmail.com>
2021-10-18 11:25:13 +0200

    classifiers

--- code/classification/run_classifier.py
+++ code/classification/run_classifier.py
@@ -10,7 +10,16 @@ Created on Wed Sep 29 14:23:48 2021
 
 import argparse, pickle
 from sklearn.dummy import DummyClassifier
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, cohen_kappa_score
+from sklearn.metrics import accuracy_score, cohen_kappa_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.pipeline import make_pipeline
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn import metrics
 
 # setting up CLI
 parser = argparse.ArgumentParser(description = "Classifier")
@@ -19,11 +28,17 @@ parser.add_argument("-s", '--seed', type = int, help = "seed for the random numb
 parser.add_argument("-e", "--export_file", help = "export the trained classifier to the given location", default = None)
 parser.add_argument("-i", "--import_file", help = "import a trained classifier from the given location", default = None)
 parser.add_argument("-m", "--majority", action = "store_true", help = "majority class classifier")
-parser.add_argument("-f", "--frequency", action = "store_true", help = "label frequency class classifier")
+parser.add_argument("-f", "--frequency", action = "store_true", help = "label frequency classifier")
+parser.add_argument("--knn", type = int, help = "k nearest neighbor classifier with the specified value of k", default = None)
+parser.add_argument("-n", "--naive_bayes", action = "store_true", help = "NaiveBayes classifier", default = None)
+parser.add_argument("-r", "--logistic_regression", action = "store_true", help = "logistic_regression classifier", default = None )
+parser.add_argument("-t", "--dicision_tree", action = "store_true", help = "dicision_tree classifier", default = None )
+parser.add_argument("-rf", "--random_forest", action = "store_true", help = "random_forest classifier", default = None )
+parser.add_argument("-svc", "--svc", action = "store_true", help = "svc classifier", default = None )
+parser.add_argument("-lsvc", "--linear_svc", action = "store_true", help = "linear_svc classifier", default = None )
 parser.add_argument("-a", "--accuracy", action = "store_true", help = "evaluate using accuracy")
-parser.add_argument("-c", "--classification", action = "store_true", help = "show classification report")
-parser.add_argument("-n", "--confusion", action = "store_true", help = "show confusion matrix")
-parser.add_argument("-k", "--kappa", action = "store_true", help = "evaluate using cohen's kappa score")
+parser.add_argument("-k", "--kappa", action = "store_true", help = "evaluate using Cohen's kappa")
+parser.add_argument("--log_folder", help = "where to log the mlflow results", default = "data/classification/mlflow")
 args = parser.parse_args()
 
 # load data
@@ -33,22 +48,75 @@ with open(args.input_file, 'rb') as f_in:
 if args.import_file is not None:
     # import a pre-trained classifier
     with open(args.import_file, 'rb') as f_in:
-        classifier = pickle.load(f_in)
+        input_dict = pickle.load(f_in)
+    
+    classifier = input_dict["classifier"]
+
 
 else:   # manually set up a classifier
     
     if args.majority:
         # majority vote classifier
         print("    majority vote classifier")
+        params = {"classifier": "majority"}
         classifier = DummyClassifier(strategy = "most_frequent", random_state = args.seed)
-        classifier.fit(data["features"], data["labels"])
         
     elif args.frequency:
-        # label frequency vote classifier
-        print("    label frequency vote classifier")
+        # label frequency classifier
+        print("    label frequency classifier")
+        params = {"classifier": "frequency"}
         classifier = DummyClassifier(strategy = "stratified", random_state = args.seed)
-        classifier.fit(data["features"], data["labels"])
         
+    
+    #K_Neighbors classifier
+    elif args.knn is not None:
+        print("    {0} nearest neighbor classifier".format(args.knn))
+        params = {"classifier": "knn", "k": args.knn}
+        standardizer = StandardScaler()
+        knn_classifier = KNeighborsClassifier(args.knn, n_jobs = -1)
+        classifier = make_pipeline(standardizer, knn_classifier)
+        
+    #NaiveBayes classifier
+    elif args.naive_bayes is not None:
+        print (" naive_bayes Classifier")
+        params = {"classifier": "naive_bayes"}
+        classifier = GaussianNB()
+        
+    #LinearRegression classifier
+    elif args.logistic_regression is not None:
+        print(" logistic_regression classifier")
+        params = {"classifier": "logistic_regression"}
+        classifier = LogisticRegression()
+        print ("penalty " +classifier.penalty)
+        
+    #dicision_tree classifier
+    elif args.dicision_tree is not None:
+        print("dicision_tree classifier")
+        params = {"classifier": "dicision_tree"}
+        classifier = DecisionTreeClassifier()
+        #print ("leaves count: "+ classifier.ccp_alpha)
+        
+    #random_forest classifier
+    elif args.random_forest is not None:
+        print ("random_forest classifier")
+        params = {"classifier" :"random_forest"}
+        classifier = RandomForestClassifier(n_estimators=100)
+        
+    #svc classifier
+    elif args.svc is not None:
+        print ("svc classifier")
+        params = {"classifier" :"svc"}
+        classifier = make_pipeline(StandardScaler(), SVC(gamma='auto'))
+        
+    #linear_svc classifier
+    elif args.linear_svc is not None:
+        print ("linear_svc classifier")
+        params = {"classifier" :"linear_svc"}
+        classifier = SVC(kernel='linear')
+        
+    
+    
+    classifier.fit(data["features"], data["labels"].ravel())
 
 # now classify the given data
 prediction = classifier.predict(data["features"])
@@ -57,19 +125,16 @@ prediction = classifier.predict(data["features"])
 evaluation_metrics = []
 if args.accuracy:
     evaluation_metrics.append(("accuracy", accuracy_score))
-if args.classification:
-    evaluation_metrics.append(("classification report", classification_report))
-if args.confusion:
-    evaluation_metrics.append(("confusion matrix", confusion_matrix))
 if args.kappa:
-    evaluation_metrics.append(("Cohen's Kappa score", cohen_kappa_score))
-
+    evaluation_metrics.append(("Cohen_kappa", cohen_kappa_score))
 
 # compute and print them
 for metric_name, metric in evaluation_metrics:
-    print("    {0}: {1}".format(metric_name, metric(data["labels"], prediction)))
+    metric_value = metric(data["labels"], prediction)
+    print("    {0}: {1}".format(metric_name, metric_value))
     
 # export the trained classifier if the user wants us to do so
 if args.export_file is not None:
+    output_dict = {"classifier": classifier, "params": params}
     with open(args.export_file, 'wb') as f_out:
-        pickle.dump(classifier, f_out)
+        pickle.dump(output_dict, f_out)
